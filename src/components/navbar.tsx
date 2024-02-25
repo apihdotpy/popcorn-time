b7e8de65621504709c820b385b73407528ee23a2
apihdotpy <74187080+apihdotpy@users.noreply.github.com>
2024-02-25 18:06:15 +0800

    Remove commented out code in navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,18 +21,3 @@ export function Navbar() {
     </header>
   )
 }
-
-{
-  /* <header className="contents lg:fixed lg:inset-0 lg:z-40 lg:flex">
-          <div className="contents dark:border-slate-50/[0.05] lg:block lg:w-72 lg:border-r lg:border-slate-900/10 xl:w-80">
-            <div className="fixed inset-0 top-0 z-50 flex h-14 items-center justify-between gap-12 bg-white/50 px-4 backdrop-blur-sm transition dark:bg-slate-900/20 dark:backdrop-blur sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80">
-              <div className="absolute inset-x-0 top-full h-px bg-slate-900/[.075] transition dark:bg-white/[.075]"></div>
-              Navbar
-            </div>
-          </div>
-        </header> */
-}
-
-///fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80 backdrop-blur-sm lg:left-72 xl:left-80 dark:backdrop-blur bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]
-
-//sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent
